ecede40e34f2e73be63b032ad7c5c799e9b18c12
MaxvanderKroon <45490765+MaxvanderKroon@users.noreply.github.com>
2020-06-19 11:34:58 +0200

    User input asked, output depends on input.

--- Enigma.py
+++ Enigma.py
@@ -1,2 +1,10 @@
-from string import ascii_lowercase
-import json
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+print(list(alphabet))
+i = 1
+while i < 6:
+  UI = input("Please enter a letter:\n")
+  print(f'You entered {UI}')
+  if UI == "A":
+    print("processing A")
+  else:  
+    print ("okay")
