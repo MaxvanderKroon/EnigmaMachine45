9838c58879a806719e53eb7324ef1e2d988213ce
MaxvanderKroon <45490765+MaxvanderKroon@users.noreply.github.com>
2020-06-26 13:49:39 +0200

    added while loop for repeating process.

--- Enigma.py
+++ Enigma.py
@@ -1,5 +1,13 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 UIlist = []
 
-UI = input("Please enter the encrypted/decrypted string:\n")
-print (list(UI))
+while True:
+  UI = input("Please enter the encrypted/decrypted string:\n")
+  print(list(UI))
+  UIlist = [UI.lower]
+
+  while True:
+    if input("Would you like to try again? please enter 'yes' or 'no'\n") == "yes":
+      break
+    else:
+      print("terminating programme")
