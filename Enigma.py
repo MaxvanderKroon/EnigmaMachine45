bd669d61ad4753c9d91128af9753b00c2e42a5e2
MaxvanderKroon <45490765+MaxvanderKroon@users.noreply.github.com>
2020-06-26 18:06:51 +0200

    Added programme termination, created class

--- Enigma.py
+++ Enigma.py
@@ -1,13 +1,25 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 UIlist = []
 
+class Enigma:
+  def MachineParts(self, rotors, steckerbrett):
+    self.rotors = rotors
+    self.steckerbrett = steckerbrett
+
 while True:
   UI = input("Please enter the encrypted/decrypted string:\n")
   print(list(UI))
-  UIlist = [UI.lower]
+  UIlist = [UI.lower()]
 
-  while True:
-    if input("Would you like to try again? please enter 'yes' or 'no'\n") == "yes":
+  i = 3
+  while i == 3:
+    TryAgain = input("Would you like to try again? please enter 'yes' or 'no'\n")
+    if TryAgain == "yes":
       break
     else:
-      print("terminating programme")
+      i = i + 1
+      if i != 3:
+        print("terminating programme...")
+  if i != 3:
+   break
+print("Programme sucessfully terminated")
