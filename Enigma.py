75a7a5aaf728664ecdea3de8f50e0da521463353
MaxvanderKroon <45490765+MaxvanderKroon@users.noreply.github.com>
2020-06-26 10:47:00 +0200

    Input changes to list

--- Enigma.py
+++ Enigma.py
@@ -1,10 +1,5 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-print(list(alphabet))
-i = 1
-while i < 6:
-  UI = input("Please enter a letter:\n")
-  print(f'You entered {UI}')
-  if UI == "A":
-    print("processing A")
-  else:  
-    print ("okay")
+UIlist = []
+
+UI = input("Please enter the encrypted/decrypted string:\n")
+print (list(UI))
